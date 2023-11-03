d87f588f93099507a663b5a01e3f89d6e00f2967
eranda-ihalagedara <67923594+eranda-ihalagedara@users.noreply.github.com>
2023-11-03 12:27:29 +1100

    Implement in forward pass in FC

--- Neural Network From Scratch/Fully_Connected.py
+++ Neural Network From Scratch/Fully_Connected.py
@@ -5,13 +5,13 @@ class Fully_Connected:
     # Set initial paramters: size of the layer(size_out), activation function (linear by defaut)
     def __init__(self, size = 1, activation='linear'):
         self.size_out = size
-        self.size_in = size
+ 
         if activation == 'ReLU':
-            self.activation = act.reLu
+            self.g = act.reLu
         else if activation == 'Sigmoid':
-            self.activation = act.sigmoid
+            self.g = act.sigmoid
         else :
-            self.activation = act.linear
+            self.g = act.linear
     
     # Set input size to the layer and initilize the weight matrix and bias vector when building the a model
     def build(self, size_in)
@@ -19,8 +19,9 @@ class Fully_Connected:
         self.w = np.random.rand(self.size_out,self.size_in)
         self.b = np.random.rand(self.size_out,1)
     
-    def forward_pass(self):
-        pass
+    def forward_pass(self, a):
+        self.z = np.matmul(self.w, a) + self.b
+        return self.g(self.z))
 
     def backward_pass(self):
         pass
