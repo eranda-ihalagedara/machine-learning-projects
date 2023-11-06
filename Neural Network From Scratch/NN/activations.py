32146f1e8c030154bd9a7825fac60cfc1cede031
eranda-ihalagedara <67923594+eranda-ihalagedara@users.noreply.github.com>
2023-11-06 17:41:57 +1100

    Implement derivation functions

--- Neural Network From Scratch/NN/activations.py
+++ Neural Network From Scratch/NN/activations.py
@@ -3,8 +3,21 @@ import numpy as np
 def relu(x):
     return max(0,x)
 
+def relu_prime(x):
+    if x >= 0:
+        return 1
+    else:
+        return 0
+
 def sigmoid(x):
     return 1/(1+np.exp(-x))
 
+def sigmoid_prime(x):
+    sig = sigmoid(x)
+    return sig(1-sig)
+
 def linear(x):
     return x
+
+def linear_prime(x):
+    return 1
