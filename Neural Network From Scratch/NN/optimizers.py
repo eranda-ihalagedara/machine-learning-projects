ce4b062c0ddc9ade3932da46c4b837525bac18c7
eranda-ihalagedara <67923594+eranda-ihalagedara@users.noreply.github.com>
2023-11-23 18:18:52 +1100

    Implement RMSProp

--- Neural Network From Scratch/NN/optimizers.py
+++ Neural Network From Scratch/NN/optimizers.py
@@ -1,11 +1,24 @@
 import numpy as np
 
 class rmsprop:
-    def __init__(self, beta, w_shape, b_shape) -> None:
+    def __init__(self, beta=0.9, w_shape, b_shape) -> None:
         self.beta = beta
         self.sdw = np.zeros(w_shape)
         self.sdb = np.zeros(b_shape)
 
+    
+    def get_dw_opt(self, dw, t):
+        self.sdw = self.beta*self.sdw + (1-self.beta)*dw**2
+        self.sdw /=(1-self.beta**t) 
+        return dw/np.sqrt(self.sdw)
+    
+
+    def get_db_opt(self, db, t):
+        self.sdb = self.beta*self.sdb + (1-self.beta)*db**2
+        self.sdb /=(1-self.beta**t)
+        return db/np.sqrt(self.sdb)
+    
+
 class adam:
-    def __init__(self) -> None:
+    def __init__(self, beta1=0.9, beta2=0.999, w_shape, b_shape) -> None:
         pass
